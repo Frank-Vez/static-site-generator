db1793973555e82e8ebe5c633a78407f38cb2f64
Frank-Vez <vezina.francis.04@gmail.com>
2024-04-19 11:37:41 -0400

    add 2 test and test file

--- src/main.py
+++ src/main.py
@@ -2,5 +2,6 @@ from textnode import Textnode
 
 def main():
   print(Textnode("This is a text node", "bold", "https://www.boot.dev"))
+  print(Textnode("This is a text node", "bold", "https://www.boot.dev"))
 
 main()
---
+++ src/test_textnode.py
@@ -0,0 +1,18 @@
+import unittest
+
+from textnode import Textnode
+
+
+class TestTextNode(unittest.TestCase):
+    def test_eq(self):
+        node = Textnode("This is a text node", "bold","https://www.boot.dev" )
+        node2 = Textnode("This is a text node", "bold","https://www.boot.dev")
+        self.assertEqual(node, node2)
+    def test_eq1(self):
+        node = Textnode("This isnt a text node", "bold","https://www.boot.dev" )
+        node2 = Textnode("This is a text node", "bold","https://www.boot.dev")
+        self.assertEqual(node, node2)
+
+
+if __name__ == "__main__":
+    unittest.main()
